8978d4906e515e0b1e271e34a1fc1deb77845885
Neha Bora <67253697+nehamutha25@users.noreply.github.com>
2026-01-14 11:20:10 +0530

    Create Task2_MathModule.py

---
+++ Task2_MathModule.py
@@ -0,0 +1,9 @@
+# Task 2: Using math module
+
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square root:", math.sqrt(num))
+print("Natural logarithm:", math.log(num))
+print("Sine (in radians):", math.sin(num))
